9c308387c5edd473ec29f09bdb7085b4c23bebc6
Manuel Durán Aguete <manuel@aguete.org>
2018-02-09 18:58:58 +0100

    Adjustments

--- src/drivers/basho_bench_driver_cs.erl
+++ src/drivers/basho_bench_driver_cs.erl
@@ -26,6 +26,7 @@
 
 -include("basho_bench.hrl").
 
+
 -record(state, {bucket}).
 
 %% ====================================================================
@@ -34,15 +35,15 @@
 
 new(_Id) ->
 	%% Ensure that erlcloud is up
-	erlcloud:start(),
+	application:ensure_all_started(erlcloud),
 
 	%% Configure erlcloud
 	AccessKey = basho_bench_config:get(cs_access_key),
 	SecretKey = basho_bench_config:get(cs_secret_key),
 	Host      = basho_bench_config:get(cs_host, "localhost"),
 	Port      = basho_bench_config:get(cs_port,  8080),
-	Protocol  = basho_bench_config:get(cs_protocol, "https"),
-	Bucket    = basho_bench_config:get(cs_bucket, "bench_test"),
+	Protocol  = basho_bench_config:get(cs_protocol, "https://"),
+	Bucket    = basho_bench_config:get(cs_bucket, "bench-test"),
 
 	erlcloud_s3:configure(AccessKey, SecretKey, Host, Port, Protocol),
 	erlcloud_s3:create_bucket(Bucket),
@@ -52,9 +53,15 @@ new(_Id) ->
 
 run(put, KeyGen, ValueGen, State) ->
 	Key = KeyGen(),
-	Value = ValueGen(),
-	erlcloud_s3:put_object(State#state.bucket, integer_to_list(Key), Value),
-	{ok, State};
+	try
+
+		Value = ValueGen(),
+		erlcloud_s3:put_object(State#state.bucket, integer_to_list(Key), Value),
+		{ok, State}
+	catch _X:_Y ->
+		?ERROR("Error on PUT ~p: ~p ~p\n", [Key, _X, _Y]),
+		{ok, State}
+	end;
 
 run(get, KeyGen, _ValueGen, State) ->
 	Key = KeyGen(),
@@ -62,17 +69,17 @@ run(get, KeyGen, _ValueGen, State) ->
 		erlcloud_s3:get_object(State#state.bucket, integer_to_list(Key)),
     	{ok, State}
 	catch _X:_Y ->
-        ?ERROR("Error on ~p: ~p ~p\n", [Key, _X, _Y]),
+        ?ERROR("Error on GET ~p: ~p ~p\n", [Key, _X, _Y]),
 		{error, object_not_found, State}
 	end;
 
 run(delete, KeyGen, _ValueGen, State) ->
+	Key = KeyGen(),
 	try
-		Key = KeyGen(),
 		erlcloud_s3:delete_object(State#state.bucket, integer_to_list(Key)),
 		{ok, State}
 	catch _X:_Y ->
-%%        ?ERROR("Error on ~p: ~p ~p\n", [Key, _X, _Y]),
+		?ERROR("Error on DELETE ~p: ~p ~p\n", [Key, _X, _Y]),
 		{ok, State}
 	end.
 
